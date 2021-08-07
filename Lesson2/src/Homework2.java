6ce13547f96902c3220a50c68d01c154633b45d7
Gerovna <gerasimovich1205@gmail.com>
2021-08-07 19:22:51 +0300

    Lesson2: домашнее задание 2

---
+++ Lesson2/src/Homework2.java
@@ -0,0 +1,11 @@
+public class Homework2 {
+    public static void main(String[] args) {
+        System.out.println(max(29, 127));
+        int a = 29;
+        int b = 127;
+    }
+    public static int max(int a, int b) {
+        System.out.println(Math.max(a, b));
+        return 1;
+    }
+}
